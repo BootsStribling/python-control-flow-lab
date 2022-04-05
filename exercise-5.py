c9651450abe288c4e5c88b10676b487d44f5f44a
BootsStribling <williamstriblingenterprises@gmail.com>
2022-04-04 19:45:45 -0500

    complete lab

---
+++ exercise-5.py
@@ -0,0 +1,50 @@
+# exercise-05 Fibonacci sequence for first 50 terms
+
+# Write the code that:
+# 1. Calculates and prints the first 50 terms of the fibonacci sequence.
+# 2. Print each term and number as follows:
+#      term: 0 / number: 0
+#      term: 1 / number: 1
+#      term: 2 / number: 1
+#      term: 3 / number: 2
+#      term: 4 / number: 3
+#      term: 5 / number: 5
+#      etc.
+
+# Hint: The next number is found by adding the two numbers before it
+
+nterms = int(input('How many terms?'))
+
+n1, n2 = 0, 1
+count = 0
+
+if nterms <= 0:
+  print('Please enter positive number')
+elif nterms == 1:
+  print(f'Sequence up to {nterms}:')
+  print(n1)
+else:
+  print('Sequence:')
+  while count < nterms:
+    print(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
+
+
+# This CODE BREAKS YOUR COMPUTER DUMMY
+# def fibonacci(n):
+#   if n <= 1:
+#     return n
+#   else:
+#     return (fibonacci(n-1) + fibonacci(n-2))
+
+# nterms = 50
+
+# if nterms <= 0:
+#   print("Please enter a positive number")
+# else:
+#   print('Sequence:')
+#   for i in range(n): 
+#     print(fibonacci(i))
---
+++ exercise-6.py
@@ -0,0 +1,36 @@
+# exercise-06 What's the  Season?
+
+# Write the code that:
+# 1. Prompts the user to enter the month (as three characters):
+#      Enter the month of the year (Jan - Dec):
+# 2. Then prompts the user to enter the day of the month: 
+#      Enter the day of the month:
+# 3. Calculate what season it is based upon this chart:
+#      Dec 21 - Mar 19: Winter
+#      Mar 20 - Jun 20: Spring
+#      Jun 21 - Sep 21: Summer
+#      Sep 22 - Dec 20: Fall
+# 4. Print the result as follows:
+#      <Mmm> <dd> is in <season> 
+
+# Hints:
+# Consider using the in operator to check if a string is in a particular list/tuple like this:
+# if input_month in ('Jan', 'Feb', 'Mar'):
+#
+# After setting the likely season, you can use another if...elif...else statement to "adjust" if
+# the day number falls within a certain range.
+
+month = input('Enter the month of the year (Jan - Dec):')
+day = int(input ('Enter the day of the month:'))
+
+
+if month in ('Dec', 'Jan', 'Feb', 'Mar'):
+  season = 'fall' if month == 'Dec' and day < 21 else 'winter'
+elif month in ('Mar', 'Apr', 'May', 'Jun'):
+  season = 'winter' if month == 'Mar' and day < 20 else 'spring'
+elif month in ('Jun', 'Jul', 'Aug', 'Sep'):
+  season = 'spring' if month == 'Jun' and day < 21 else 'summer'
+if month in ('Sep', 'Oct', 'Nov', 'Dec'):
+  season = 'summer' if month == 'Sep' and day < 22 else 'fall'
+
+print(f'{month} {day} is in the {season}')
